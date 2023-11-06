4534ebc6c8c2f6b61643c0d737be8c463d663d9d
lucasgiosso <lucasdiazgiosso@gmail.com>
2023-11-05 23:55:03 -0300

    Inicializando...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './modules/general/login/login.component';
+import { RegisterComponent } from './modules/general/register/register.component';
+import { HomeComponent } from './modules/general/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
